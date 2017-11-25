d43dd93e10f297e95b4ab9c76f4307eca19eed8d
Matt-Lepley <matt@thefirehoseproject.com>
2017-11-25 09:59:53 -0500

    Powerup starting

--- headers/powerup.h
+++ headers/powerup.h
@@ -1,14 +1,24 @@
 #ifndef POWERUP_H
 #define POWERUP_H
 
+#include "globals.h"
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
 class Powerup {
 
 public:
+  void init(int val, int duration, string file);
+  void timeleft();
+  void clean();
 
 private:
   int value; // Global enum
-  
-
+  int timerDuration;
+  int timerStart;
+  Mus_Chunk *sound = NULL;
+  bool hasPowerup = false;
 };
 
 #endif
--- src/powerup.cpp
+++ src/powerup.cpp
@@ -1,1 +1,20 @@
 #include "powerup.h"
+
+void Powerup::init(int val, int duration, string file) {
+  value = val;
+  timerDuration = duration;
+  sound = Mix_LoadWAV(file.to_cstr());
+  Mix_PlayChannel(-1, sound, 0);
+}
+
+int Powerup::timeleft() {
+  if(timerStart + timerDuration > SDL_GetTicks()) {
+    return (timerStart + timerDuration) - SDL_GetTicks();
+  } else {
+    return 0;
+  }
+}
+
+void Powerup::clean() {
+  Mix_FreeChunk(sound);
+}
